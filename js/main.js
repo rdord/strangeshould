f75b00d3fc87d33c4d2be2e3ecd2c16026151cd8
Roland <rdorde@gmail.com>
2016-11-19 21:00:41 +0100

    feat: new version

--- js/main.js
+++ js/main.js
@@ -27,10 +27,35 @@ function specialConcat (array1, array2) {
   return arrayOut
 }
 
+function pageUnload(event) {
+  event.preventDefault()
+
+  var url = this.getAttribute('href')
+  var body = select('body')
+  body.removeClass('is-animating')
+  body.offsetWidth
+  body.addClass('is-animating')
+
+  setTimeout(function() {
+    window.location.href = url
+  }, 800)
+}
 
 
 function setup () {
   noCanvas()
+  // var loadingOverlay = select('.Guard')
+  var audio = new Audio('../assets/blop-quiet.mp3');
+
+  // setTimeout(function() {
+  //   loadingOverlay.addClass('hide')
+  // }, 600)
+
+  setTimeout(function() {
+    // loadingOverlay.addClass('hide')
+    audio.play()
+  }, 120)
+
   var howMuch = 1000
   var moods = getManyData(moodData.moods, howMuch)
   var bodyParts = getManyData(bodyPartData.bodyParts, howMuch)
@@ -38,12 +63,14 @@ function setup () {
 
   // console.log(moods)
   // console.log(bodyParts)
-  console.log(moodyParts)
+  // console.log(moodyParts)
 
   var parentDiv = document.getElementById('InnerMouth')
   var p = createP(moodyParts)
   p.parent(parentDiv)
 
-
   noLoop()
+
+  var listLink = document.querySelector('.Mouthpiece-essence');
+  listLink.addEventListener("click", pageUnload);
 }
